4e4110b3f8ffd5cb7a7670798d1e94556f598804
Louay-daher1 <louaydaher173@gmil.com>
2023-07-06 23:09:03 +0300

    first commit

---
+++ Api.js
@@ -0,0 +1,23 @@
+const express = require('express')
+var bodyParser = require('body-parser');
+const cors=require('cors');
+const xlsx = require('xlsx');
+
+
+const app = express()
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: false }))
+
+app.use(cors());
+
+app.get('/', function (req, res) {
+  res.send (" Hello world")
+})
+app.get('/louay', function (req, res) {
+   let wb = xlsx.readFile('Data.xlsx');
+    let ws = wb.Sheets['Students'];
+    let Data = xlsx.utils.sheet_to_json(ws) ; 
+    res.send(Data);
+  })
+
+app.listen(3000);
